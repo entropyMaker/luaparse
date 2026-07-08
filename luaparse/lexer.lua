0cacaadcf3ccfb797016a0f629153176dae2d0e4
Juntun <juntobov@hotmail.com>
2026-07-08 23:44:39 +0800

    add scan_number

--- luaparse/lexer.lua
+++ luaparse/lexer.lua
@@ -49,7 +49,80 @@ local function scan_identifier(input, index)
     i = i + 1
   end
 
-  return i - 1
+  return i
+end
+
+-- based on regexp '(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?'
+-- \d (i.e. [0-9]) is alphabet 1
+-- \. (i.e. the literal ".") is alphabet 2
+-- [eE] (i.e. "e" or "E" for exponent) is alphabet 3
+-- [-+] (i.e. the positive or negative sign "+" or "-") is alphabet 4
+-- accept states: 2, 4, 6, 7
+local decimal_trans_table = {
+  {2, 3, 0, 0},
+  {2, 4, 5, 0},
+  {6, 0, 0, 0},
+  {4, 0, 5, 0},
+  {7, 0, 0, 8},
+  {6, 0, 5, 0},
+  {7, 0, 0, 0},
+  {7, 0, 0, 0},
+}
+
+local function starts_with_0x(input, index)
+  if index >= #input or byte(input, index) ~= 48 --[["0"]] then
+    return false
+  end
+
+  local second = byte(input, index+1)
+  return second == 88 or second == 120 -- "x" or "X"
+end
+
+local function is_hex_char(char)
+  return (48 <= char and char <= 57) -- 0-9
+      or (65 <= char and char <= 70) -- A-F
+      or (97 <= char and char <= 102) -- a-f
+end
+
+local function scan_number(input, index)
+  local length = #input
+
+  if starts_with_0x(input, index) then
+    local i = index + 2
+    while i <= length and is_hex_char(byte(input, i)) do i = i + 1 end
+    return i > index + 2 and i or index
+  end
+
+  local state = 1
+  local last_accept = index
+  for i = index, length do
+    local alphabet = 0
+    local b = byte(input, i)
+    if 48 <= b and b <= 57 then -- 0-9
+      alphabet = 1
+    elseif b == 46 then -- "."
+      alphabet = 2
+    elseif b == 69 or b == 101 then -- "E" or "e"
+      alphabet = 3
+    elseif b == 43 or b == 45 then -- "+" or "-"
+      alphabet = 4
+    end
+
+    if alphabet == 0 then
+      break
+    end
+
+    state = decimal_trans_table[state][alphabet]
+    if state == 0 then
+      break
+    end
+
+    if state ==  2 or state ==  4 or state ==  6 or state ==  7 then
+      last_accept = i + 1
+    end
+  end
+
+  return last_accept
 end
 
 return lexer
